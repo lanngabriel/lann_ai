dbd6ffc678601a5287b1509ce0a090f87bdd6e62
lanngabriel <lannsoquiat07@gmail.com>
2025-01-10 02:01:20 +0800

    first commit

--- lib/help/global.dart
+++ lib/help/global.dart
@@ -5,4 +5,3 @@ const appName = 'AI Playground';
 late Size mq;
 
 const apiKey = 'AIzaSyCyzzeiSJEJ7sD3oefjSS5q50613KMdh7g';
-//const apiKey = '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';
